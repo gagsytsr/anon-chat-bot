ce4a214f3aab304de7c3c136225a69f7be6e772f
gagsytsr <tyred882@gmail.com>
2025-09-05 13:28:35 +0200

    Update config.py

--- config.py
+++ config.py
@@ -13,10 +13,10 @@ if not all([BOT_TOKEN, ADMIN_PASSWORD, DATABASE_URL]):
 # --- Константы бота ---
 REWARD_FOR_REFERRAL = 10
 COST_FOR_18PLUS = 50
-COST_FOR_UNBAN = 100
+COST_FOR_UNBAN = 50  # Изменено со 100 на 50
 COST_FOR_PHOTO = 50
 MAX_WARNINGS = 3
-CHAT_TIMER_SECONDS = 60  # Время в секундах до предложения об обмене никами (60 сек = 1 мин)
+CHAT_TIMER_SECONDS = 60
 
 ADMIN_IDS = set()
 
